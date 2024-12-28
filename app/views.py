caad290a0b23a3476fb0dcdce1156b6f1d1cc481
StudentBI14 <StudentBI14@SYN13ASS-NB-539.alabuga.local>
2024-12-28 15:16:46 +0300

    feat: close #2

--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+# views.py
+from django.shortcuts import render
+from django.db.models import Q
+from .models import Product
+from .forms import SearchForm
+
+def search(request):
+    form = SearchForm()
+    results = []
+
+    if 'name' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            results = Product.objects.filter(name= name)
+
+    return render(request, 'main.html', {'form': form, 'results': results})
