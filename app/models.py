c1acba0457df591d4f583971ca6b3bf45c53d2d2
StudentBI11 <StudentBI11@SYN13ASS-NB-511.alabuga.local>
2024-12-28 15:02:27 +0300

    feat: close #1

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    describe = models.CharField(max_length=255)
+    price = models.IntegerField()
+    number = models.IntegerField()
+
+
+class Sklad(models.Model):
+    name = models.IntegerField()
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+class Dostavka(models.Model):
+    city = models.CharField(max_length=255)
+    city_off = models.ForeignKey(Sklad, on_delete=models.CASCADE)
+    cost_of_dostavka = models.IntegerField()
